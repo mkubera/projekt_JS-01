3714c8194df0d9da65b21c4d124d124b4bfb30f7
mkubera <rainteller@tutanota.de>
2021-11-01 15:01:42 +0100

    Added spinner

--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,10 @@
 import Utils from "./utils";
 import { v4 as uuidv4 } from "uuid";
+import { Spinner } from "spin.js";
+
+const newSpinner = new Spinner({ color: "#fff", lines: 12 });
+
+console.log(newSpinner);
 
 console.log(uuidv4());
 
